0b94f0f74f4f4f961b845db2fcc1b5d694306bc2
Túlio Barbosa <tuliorbarbosa@gmail.com>
2026-04-21 16:44:27 +0100

    Correcao do nome do bloco da vnet2

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ resource "azurerm_virtual_network" "vnet-jenkins" {
   address_space       = ["10.0.0.0/16"]
 }
 
-resource "azurerm_virtual_network" "vnet-j2" {
+resource "azurerm_virtual_network" "vnet-2" {
   name                = "VNET-2"
   resource_group_name = azurerm_resource_group.rg-jenkins.name
   location            = azurerm_resource_group.rg-jenkins.location
