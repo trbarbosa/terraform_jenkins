ce25d92891af0ba0edc450efc2d04aabba563b55
Túlio Barbosa <tuliorbarbosa@gmail.com>
2026-04-21 17:02:03 +0100

    Renomeando o bloco da segunda vnet

--- main.tf
+++ main.tf
@@ -10,14 +10,14 @@ resource "azurerm_virtual_network" "vnet-jenkins" {
   address_space       = ["10.0.0.0/16"]
 }
 
-resource "azurerm_virtual_network" "vnet-2-mudado" {
+resource "azurerm_virtual_network" "vnet-2" {
   name                = "VNET-2"
   resource_group_name = azurerm_resource_group.rg-jenkins.name
   location            = azurerm_resource_group.rg-jenkins.location
   address_space       = ["10.0.0.0/16"]
 }
 
-#moved {
-#  from = azurerm_virtual_network.vnet-2
-#  to   = azurerm_virtual_network.vnet-2-mudado
-#}
+moved {
+  from = azurerm_virtual_network.vnet-2-mudado
+  to   = azurerm_virtual_network.vnet-2
+}
