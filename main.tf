d39136f3beb26c2ac58015951cc06af713c0b25b
Túlio Barbosa <tuliorbarbosa@gmail.com>
2026-04-21 14:48:16 +0100

    Remocao de 2 subnets desnecessarias

--- main.tf
+++ main.tf
@@ -9,17 +9,3 @@ resource "azurerm_virtual_network" "vnet-jenkins" {
   location            = azurerm_resource_group.rg-jenkins.location
   address_space       = ["10.0.0.0/16"]
 }
-
-#resource "azurerm_subnet" "subnet-1" {
-#  name                 = "SUBNET-1"
-#  resource_group_name  = azurerm_resource_group.rg-teste.name
-#  virtual_network_name = azurerm_virtual_network.vnet-teste.name
-#  address_prefixes     = ["10.0.0.0/24"]
-#}
-
-#resource "azurerm_subnet" "subnet-2" {
-#  name                 = "SUBNET-2"
-#  resource_group_name  = azurerm_resource_group.rg-teste.name
-#  virtual_network_name = azurerm_virtual_network.vnet-teste.name
-#  address_prefixes     = ["10.0.3.0/24"]
-#}
